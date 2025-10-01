20f77a91f93b42183928ecb2c7e0acaaaf1d016f
Monali Gupta <monaligupta06@gmail.com>
2025-10-01 15:54:20 +0530

    second

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
-const ToDoList = ({todos, setTodos, handleComplete}) => {
+const ToDoList = ({ todos, setTodos, handleComplete }) => {
   return (
     <div>
         <h2>Child Component</h2>
         <ul>
-            {todos.map((item, index)=>{
-                return <li>{item.text}{!item.isComplete && <button onClick={() => {
-                  handleComplete(index)
-                }}>Complete</button>}</li>
+            {todos.map((item, index) => {
+                return (
+                  <li key={index}>
+                    {item.text}
+                    {!item.isComplete && (
+                      <button onClick={() => handleComplete(index)}>
+                        Complete
+                      </button>
+                    )}
+                  </li>
+                )
             })}
         </ul>
     </div>
